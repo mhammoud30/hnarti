d37b368552a05354841a471e7c73364273779fbc
mhammoud30 <78760617+mhammoud30@users.noreply.github.com>
2024-08-15 14:02:48 +0300

    Create basic structure for custom theme

---
+++ wp-content/themes/hnarti/functions.php
@@ -0,0 +1,6 @@
+<?php
+   function custom_theme_setup() {
+       add_theme_support( 'title-tag' );
+       add_theme_support( 'post-thumbnails' );
+   }
+   add_action( 'after_setup_theme', 'custom_theme_setup' );
